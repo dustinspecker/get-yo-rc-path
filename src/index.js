b3b4b26cf302cb9995941a8a5770968b6ad391a3
Dustin Specker <dustinspecker@dustinspecker.com>
2016-02-06 13:25:00 -0600

    chore(src): use arrow functions

--- src/index.js
+++ src/index.js
@@ -6,15 +6,12 @@ import findUp from 'find-up'
  * Returns full path of .yo-rc.json or null if not found
  * @return {Promise<String | null>} - full path
  */
-module.exports = function () {
-  return findUp('.yo-rc.json')
-}
+module.exports = () => findUp('.yo-rc.json')
 
 /**
  * Returns directory of .yo-rc.json or null if not found
  * @return {Promise<String | null>} - directory
  */
-module.exports.dir = function () {
-  return module.exports()
+module.exports.dir = () =>
+  module.exports()
     .then(file => file && dirname(file))
-}
