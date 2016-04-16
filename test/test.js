f43a615024056f92d2b09c403c59fa4dc2bfbba3
Dustin Specker <DustinSpecker@DustinSpecker.com>
2016-04-16 12:08:23 -0500

    chore(deps): update eslint-config-dustinspecker
    Closes #24

--- test/test.js
+++ test/test.js
@@ -8,7 +8,9 @@ import proxyquire from 'proxyquire'
 
 describe('get-yo-rc-path', () => {
   it('should return null if .yo-rc.json file not found', async () => {
-    const findUpStub = austin.spy().withArgs('.yo-rc.json').returns(Promise.resolve(null))
+    const findUpStub = austin.spy()
+        .withArgs('.yo-rc.json')
+          .returns(Promise.resolve(null))
       , getYoRcPathProxy = proxyquire('../lib/', {'find-up': findUpStub})
 
     expect(await getYoRcPathProxy()).to.eql(null)
@@ -16,14 +18,18 @@ describe('get-yo-rc-path', () => {
   })
 
   it('should return full path of .yo-rc.json', async () => {
-    const findUpStub = austin.spy().withArgs('.yo-rc.json').returns(Promise.resolve('awesome-project/.yo-rc.json'))
+    const findUpStub = austin.spy()
+        .withArgs('.yo-rc.json')
+          .returns(Promise.resolve('awesome-project/.yo-rc.json'))
       , getYoRcPathProxy = proxyquire('../lib/', {'find-up': findUpStub})
 
     expect(await getYoRcPathProxy()).to.eql('awesome-project/.yo-rc.json')
   })
 
   it('should return directory of .yo-rc.json', async () => {
-    const findUpStub = austin.spy().withArgs('.yo-rc.json').returns(Promise.resolve('awesome-project/.yo-rc.json'))
+    const findUpStub = austin.spy()
+        .withArgs('.yo-rc.json')
+          .returns(Promise.resolve('awesome-project/.yo-rc.json'))
       , getYoRcPathProxy = proxyquire('../lib/', {'find-up': findUpStub})
 
     expect(await getYoRcPathProxy.dir()).to.eql('awesome-project')
